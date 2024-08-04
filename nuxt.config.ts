ed8ee56504a1665b99ca7443c8cfb450f28971b9
sina khaghani <sina.it91@gmail.com>
2024-08-04 21:24:11 +0330

    create nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  devServer: {
+    host: '192.168.1.5',
+    port: 3000,
+    // بقیه تنظیمات شما
+  }
+})
